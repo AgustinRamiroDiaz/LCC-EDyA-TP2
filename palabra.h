633d72b76c3e2d5075130d6de4a708fc1dadd665
AgustinRamiroDiaz <49416765+AgustinRamiroDiaz@users.noreply.github.com>
2019-05-29 23:01:12 -0300

    Update palabra.h

--- palabra.h
+++ palabra.h
@@ -43,7 +43,7 @@ Palabra * copiarPalabra(Palabra palabra);
 
 void liberarPalabra(Palabra * palabra);
 
-ListaDePalabras * separarPalabra(Palabra palabra, int posicion)
+ListaDePalabras * separarPalabra(Palabra palabra, int posicion);
 
 
 #endif
