49c38c8018babb7fd3e1b2abfbc8d76cee501f5f
Alejandro De Cicco <alejandrodecicco99@gmail.com>
2019-06-02 22:45:17 -0300

    nombres de archivo const

--- corrector.c
+++ corrector.c
@@ -6,28 +6,28 @@
 
 FILE * obtenerArchivoDeEntrada(int argc, char const *argv[])
 {
-    char * nombreArchivoDeEntrada;
+    char const * nombreArchivoDeEntrada;
 
     if (argc > 1) {
         nombreArchivoDeEntrada = argv[1];
     } else {
         nombreArchivoDeEntrada = ARCHIVO_TEXTO_ENTRADA_POR_DEFECTO;
     }
-    FILE * archivoDeEntrada = abrirArchivo(nombreArchivoDeEntrada, "r");
+    FILE * archivoDeEntrada = abrirArchivo((char *) nombreArchivoDeEntrada, "r");
 
     return archivoDeEntrada;
 }
 
 FILE * obtenerArchivoDeCorrecciones(int argc, char const *argv[])
 {
-    char * nombreArchivoDeCorrecciones;
+    char const * nombreArchivoDeCorrecciones;
 
     if (argc > 2) {
         nombreArchivoDeCorrecciones = argv[2];
     } else {
         nombreArchivoDeCorrecciones = ARCHIVO_CORRECCIONES_POR_DEFECTO;
     }
-    FILE * archivoDeCorrecciones = abrirArchivo(nombreArchivoDeCorrecciones, "w");
+    FILE * archivoDeCorrecciones = abrirArchivo((char *) nombreArchivoDeCorrecciones, "w");
 
     return archivoDeCorrecciones;
 }
@@ -36,7 +36,7 @@ int main(int argc, char const *argv[])
 {
     setlocale(LC_ALL, "");
 
-    FILE * archivoDeTablaHash = abrirArchivo(ARCHIVO_TABLA_HASH, "r");
+    FILE * archivoDeTablaHash = abrirArchivo((char *) ARCHIVO_TABLA_HASH, "r");
     FILE * archivoDeEntrada = obtenerArchivoDeEntrada(argc, argv);
     FILE * archivoDeCorrecciones = obtenerArchivoDeCorrecciones(argc, argv);
 
