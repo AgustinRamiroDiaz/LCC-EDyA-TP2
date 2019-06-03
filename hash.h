c0599a3ed21aca598cb635302bcbc25ef6695357
AgustinRamiroDiaz <49416765+AgustinRamiroDiaz@users.noreply.github.com>
2019-06-02 21:48:40 -0300

    coments

--- hash.h
+++ hash.h
@@ -3,12 +3,19 @@
 
 #include "palabra.h"
 
+//Principal funcion de hasheo que se usa en la primer instancia
+//para generar los buckets
 unsigned long funcionHashPrincipal(Palabra palabra);
 
+//Funcion de hasheo
 unsigned long djb2(Palabra palabra);
 
+//Funcion de hasheo secundaria la cual se aplica a los buckets
+//generados a partir de la funcion de hash principal
+//con el fin de ser un hash perfecto
 unsigned long funcionHashSecundaria(Palabra palabra, unsigned long constante);
 
+//Funcion de hasheo
 unsigned long sdbm(Palabra palabra);
 
 #endif
